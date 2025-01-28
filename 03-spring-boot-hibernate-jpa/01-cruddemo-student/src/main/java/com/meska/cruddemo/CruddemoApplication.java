6278680ead72419e97c8e372092e636be6ec41f9
Yousef Meska <mrbomberboy123@gmail.com>
2025-01-28 02:28:49 +0200

    added initial hibernate-jpa

---
+++ 03-spring-boot-hibernate-jpa/01-cruddemo-student/src/main/java/com/meska/cruddemo/CruddemoApplication.java
@@ -0,0 +1,23 @@
+package com.meska.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> { // java lambda expression
+			System.out.println("Hello I run after all beans are loaded!"); // will other custom code!
+		};
+	}
+
+
+}
